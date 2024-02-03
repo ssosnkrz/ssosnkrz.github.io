dd6dfaed8d0f17201538c72e3e211780c30cc8a0
Krzysztof Sosnowski <Krzysztof.Sosnowski@transcom.com>
2024-02-03 12:04:14 +0100

    netomi

---
+++ netomi/index.js
@@ -0,0 +1,117 @@
+var ns = document.createElement("script");
+ns.src = "https://chatapps-qa.netomi.com/app/script.js";
+ns.type = "text/javascript";
+ns.async = true;
+ns.setAttribute("botRefId", "355a0d8e");
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(ns, s);
+ns.onload = function () {
+  function onLoad() {
+    Netomi.chat.onLoad(() => {
+      // This method is helpful to run any other PubSub event upon MQTT connection establishment.
+      document.querySelector(".onLoad").innerText =
+        "onLoad - This method runs when a successful MQTT connection is established.";
+      console.log(
+        "This method runs when a successful MQTT connection is established."
+      );
+    });
+  }
+  function init() {
+    Netomi.chat.init(() => {
+      // This method is helpful to run any other PubSub event upon bot has initialised with bot's json.
+      document.querySelector(".init").innerText =
+        "init - This method runs when bot has initialised with bot's json.";
+      console.log("This method runs when bot has initialised with bot's json.");
+    });
+  }
+  init();
+  onLoad();
+};
+function changeLocale() {
+  // To change the language of the bot, pass a language code in the changeLocale method.
+  const language = "fr";
+  Netomi.chat.changeLocale(language);
+}
+function openWidget() {
+  // To open the chat widget
+  Netomi.chat.openWidget();
+}
+function closeWidget() {
+  // To close the chat widget
+  Netomi.chat.closeWidget();
+}
+function showCalloutBubble() {
+  // To hide the callout bubbles
+  Netomi.chat.showCalloutBubble(false);
+}
+function showDifferentCalloutBubble() {
+  // To dynamically send a different callout bubble. It also overrides the existing callout bubbles in the bot's json.
+  const obj = {
+    url: "https://formnetomi.000webhostapp.com/testbotaakash.html",
+    calloutBubbleEnabled: true,
+    delayForFirstBubble: 1000,
+    delayInBetweenBubbles: 1000,
+    attachments: [
+      {
+        type: "Text",
+        text: "Hi!!",
+      },
+      {
+        type: "Text",
+        text: "This is netomi bot.",
+      },
+      {
+        type: "Text",
+        text: "This is netomi bot123",
+        quickReply: {
+          options: [
+            {
+              label: "form response",
+              metadata: "video response",
+              contentType: "text",
+            },
+            {
+              label: "form response",
+              metadata: "agent",
+              contentType: "text",
+            },
+          ],
+        },
+      },
+    ],
+    config: {
+      displayTimer: 10000,
+      maxWidthForMobile: "50%",
+      __exposedFnWidthRange: [0, 999999],
+    },
+  };
+  Netomi.chat.showCalloutBubble(obj);
+}
+function showWidget() {
+  // To show the widget
+  Netomi.chat.showWidget();
+}
+function hideWidget() {
+  // To completely hide the widget
+  Netomi.chat.hideWidget();
+}
+function OpenInFullScreen() {
+  // To open the widget in the full available width of the screen
+  Netomi.chat.OpenInFullScreen();
+}
+function OpenInStandardScreen() {
+  // To open the widget with default height and width.
+  Netomi.chat.OpenInStandardScreen();
+}
+function sendMessageInCW() {
+  // To send a message in the widget without typing in the footer.
+  Netomi.chat.publish("SEND_MESSAGE_IN_WIDGET", { text: "hey", label: "hey" });
+}
+function endSession() {
+  // To end the current ongoing session.
+  Netomi.chat.publish("END_SESSION");
+}
+function sendVisitorInfo() {
+  // To send visitor info as custom attributes in webhook-message API.
+  Netomi.chat.publish("VISITOR_INFO", { name: "John", org: "Netomi" });
+}
